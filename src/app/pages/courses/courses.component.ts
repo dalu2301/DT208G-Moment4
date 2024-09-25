763aea3d6ded4885bf0c545fa85d700b1b85e223
dalu2301 <dalu2301@student.miun.se>
2024-09-25 10:39:01 +0200

    Finished working version of Ascending and Descending sort of table per column

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { CourseService } from '../../services/course.service'
 import { Course } from '../../models/course'
 import { CommonModule } from '@angular/common'
@@ -14,17 +14,86 @@ import { CommonModule } from '@angular/common'
 })
 export class CoursesComponent implements OnInit {
 
+  unsortedCourses: Course[] = []
   courses: Course[] = []
+  sortedCode: string = 'ascending'
+  sortedName: string = 'ascending'
+  sortedProg: string = 'ascending'
 
-  constructor(private courseService: CourseService ) {}
+  constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
-    
+
     this.courseService.getCourses().subscribe((courses) => {
 
+      // Den Array som kommer att manipuleras
       this.courses = courses
+      // Den Array som kommer att agera återställning
+      this.unsortedCourses = courses
 
     })
+
+  }
+
+  
+  sortTable(column: string): void {
+
+    switch (column) {
+      case 'code':
+
+        if (this.sortedCode === 'ascending') {
+          
+          this.courses = this.unsortedCourses.sort((a, b) => (a.code.toLowerCase() < b.code.toLowerCase() ? -1 : 1))
+
+          this.sortedCode = 'descending'
+          
+        } else {
+          
+          this.courses = this.unsortedCourses.sort((a, b) => (a.code.toLowerCase() < b.code.toLowerCase() ? 1 : -1))
+
+          this.sortedCode = 'ascending'
+
+        }
+
+        break
+      case 'name':
+
+        if (this.sortedName === 'ascending') {
+            
+          this.courses = this.unsortedCourses.sort((a, b) => (a.coursename.toLowerCase() < b.coursename.toLowerCase() ? -1 : 1))
+
+          this.sortedName = 'descending'
+          
+        } else {
+          
+          this.courses = this.unsortedCourses.sort((a, b) => (a.coursename.toLowerCase() < b.coursename.toLowerCase() ? 1 : -1))
+
+          this.sortedName = 'ascending'
+
+        }
+        
+        break
+      case 'prog':
+
+        if (this.sortedProg === 'ascending') {
+              
+          this.courses = this.unsortedCourses.sort((a, b) => (a.progression.toLowerCase() < b.progression.toLowerCase() ? -1 : 1))
+
+          this.sortedProg = 'descending'
+          
+        } else {
+          
+          this.courses = this.unsortedCourses.sort((a, b) => (a.progression.toLowerCase() < b.progression.toLowerCase() ? 1 : -1))
+
+          this.sortedProg = 'ascending'
+
+        }
+        
+        break
+      default:
+        // Felaktigt val, ignorera...
+        break
+    }
 
   }
 
