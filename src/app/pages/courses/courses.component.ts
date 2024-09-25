b2e5f5299c0a9ca161e634c84ef369983bf1c730
dalu2301 <dalu2301@student.miun.se>
2024-09-25 14:27:04 +0200

    Use of Enum instead of hard coded values for Filter

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -4,6 +4,11 @@ import { Course } from '../../models/course'
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 
+enum Filter {
+  'Ascending' = 0,
+  'Descending' = 1
+}
+
 @Component({
   selector: 'app-courses',
   standalone: true,
@@ -14,13 +19,14 @@ import { FormsModule } from '@angular/forms'
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
+
 export class CoursesComponent implements OnInit {
 
   unsortedCourses: Course[] = []
   courses: Course[] = []
-  sortedCode: string = 'ascending'
-  sortedName: string = 'ascending'
-  sortedProg: string = 'ascending'
+  sortedCode: number = Filter.Ascending
+  sortedName: number = Filter.Ascending
+  sortedProg: number = Filter.Ascending
   filterValue: string = ''
 
   constructor(private courseService: CourseService) { }
@@ -53,51 +59,51 @@ export class CoursesComponent implements OnInit {
     switch (column) {
       case 'code':
 
-        if (this.sortedCode === 'ascending') {
+        if (this.sortedCode === Filter.Ascending) {
 
           this.courses = this.unsortedCourses.sort((a, b) => (a.code.toLowerCase() < b.code.toLowerCase() ? -1 : 1))
 
-          this.sortedCode = 'descending'
+          this.sortedCode = Filter.Descending
 
         } else {
 
           this.courses = this.unsortedCourses.sort((a, b) => (a.code.toLowerCase() < b.code.toLowerCase() ? 1 : -1))
 
-          this.sortedCode = 'ascending'
+          this.sortedCode = Filter.Ascending
 
         }
 
         break
       case 'name':
 
-        if (this.sortedName === 'ascending') {
+        if (this.sortedName === Filter.Ascending) {
 
           this.courses = this.unsortedCourses.sort((a, b) => (a.coursename.toLowerCase() < b.coursename.toLowerCase() ? -1 : 1))
 
-          this.sortedName = 'descending'
+          this.sortedName = Filter.Descending
 
         } else {
 
           this.courses = this.unsortedCourses.sort((a, b) => (a.coursename.toLowerCase() < b.coursename.toLowerCase() ? 1 : -1))
 
-          this.sortedName = 'ascending'
+          this.sortedName = Filter.Ascending
 
         }
 
         break
       case 'prog':
 
-        if (this.sortedProg === 'ascending') {
+        if (this.sortedProg === Filter.Ascending) {
 
           this.courses = this.unsortedCourses.sort((a, b) => (a.progression.toLowerCase() < b.progression.toLowerCase() ? -1 : 1))
 
-          this.sortedProg = 'descending'
+          this.sortedProg = Filter.Descending
 
         } else {
 
           this.courses = this.unsortedCourses.sort((a, b) => (a.progression.toLowerCase() < b.progression.toLowerCase() ? 1 : -1))
 
-          this.sortedProg = 'ascending'
+          this.sortedProg = Filter.Ascending
 
         }
 
